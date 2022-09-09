c17d9db09040b7fadb38103feb63cb518207940b
Guowei Zhang <guowei.zhang@shopee.com>
2022-09-09 12:48:10 +0800

    remove useless code

--- initial/src/main/java/com/example/springboot/bean/CustomService.java
+++ initial/src/main/java/com/example/springboot/bean/CustomService.java
@@ -3,10 +3,6 @@ package com.example.springboot.bean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.management.MBeanServer;
-import javax.management.ObjectName;
-import java.lang.management.ManagementFactory;
-
 @Service
 public class CustomService
 {
@@ -18,18 +14,6 @@ public class CustomService
 
     @Autowired
     public CustomMetrics customMetrics;
-
-    public CustomService()
-    {
-        try {
-            MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-            ObjectName name = new ObjectName("com.example.springboot.bean:name=customBean");
-            mBeanServer.registerMBean(customBean, name);
-        }
-        catch (Exception e) {
-            System.out.println("Error when : " + e.getMessage());
-        }
-    }
 
     public String getServiceName()
     {
