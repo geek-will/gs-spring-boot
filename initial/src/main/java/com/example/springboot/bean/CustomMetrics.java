6e3261348ef564addf46f4360c1673e88343acc6
Guowei Zhang <guowei.zhang@shopee.com>
2022-09-14 15:16:16 +0800

    update

--- initial/src/main/java/com/example/springboot/bean/CustomMetrics.java
+++ initial/src/main/java/com/example/springboot/bean/CustomMetrics.java
@@ -1,18 +1,33 @@
 package com.example.springboot.bean;
 
 import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedMetric;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.jmx.support.MetricType;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @ManagedResource
 public class CustomMetrics
 {
-    @ManagedOperation
+    private String customMetricsName = "custom_metrics_name";
+
+    @ManagedMetric
     public String getCustomMetricsName()
     {
-        return "custom_metrics_name";
+        return customMetricsName;
+    }
+
+    @ManagedOperation
+    public Map<String, String> myMetrics()
+    {
+        Map<String, String> map = new HashMap<>();
+        map.put("customMetricsName", "custom_metrics_name");
+        return map;
     }
 
     @ManagedAttribute
