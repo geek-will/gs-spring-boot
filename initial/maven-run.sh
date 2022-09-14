74139cbd4c11de6db230d9927f678810d89e2042
Guowei Zhang <guowei.zhang@shopee.com>
2022-09-14 11:18:06 +0800

    update Makefile & felete useless script

--- initial/maven-run.sh
+++
@@ -1,3 +0,0 @@
-#export MAVEN_OPTS="-javaagent:./lib/jmx_prometheus_javaagent-0.15.0.jar=8082:./lib/jmx-exporter.yaml -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
-export MAVEN_OPTS="-javaagent:./lib/jmx_prometheus_javaagent-0.12.0.jar=8082:./lib/jmx-exporter.yaml"
-./mvnw spring-boot:run -Dspring-boot.run.profiles=dev
