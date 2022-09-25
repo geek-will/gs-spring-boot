d6042ff5b8612fdea3eb87eded56fcea18694c24
Guowei Zhang <guowei.zhang@shopee.com>
2022-09-25 12:53:37 +0800

    update

--- initial/src/main/java/com/example/springboot/bean/CustomMetrics.java
+++ initial/src/main/java/com/example/springboot/bean/CustomMetrics.java
@@ -2,15 +2,12 @@ package com.example.springboot.bean;
 
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedMetric;
+import org.springframework.jmx.export.annotation.ManagedNotification;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
-import org.springframework.jmx.export.assembler.AbstractMBeanInfoAssembler;
 import org.springframework.jmx.support.MetricType;
 import org.springframework.stereotype.Component;
-
-import java.lang.management.MemoryPoolMXBean;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.jmx.support.JmxUtils;
 
 @Component
 @ManagedResource
@@ -19,33 +16,15 @@ public class CustomMetrics
     private String customMetricsName = "custom_metrics_name";
     private int customMetricsValue = 888;
 
-    @ManagedAttribute(defaultValue="custom_metrics_name", persistPeriod=300)
+    @ManagedAttribute
     public String getCustomMetricsName()
     {
         return customMetricsName;
     }
 
     @ManagedAttribute
-    public void setCustomMetricsName(String customMetricsName) {
-        this.customMetricsName = customMetricsName;
-    }
-
-    @ManagedOperation
-    public Map<String, String> myMetrics()
-    {
-        Map<String, String> map = new HashMap<>();
-        map.put("customMetricsName", "custom_metrics_name");
-        return map;
-    }
-
-    @ManagedAttribute
     public Integer getCustomMetricsValue()
     {
         return customMetricsValue;
-    }
-
-    @ManagedAttribute
-    public void setCustomMetricsValue(Integer customMetricsValue) {
-        this.customMetricsValue = customMetricsValue;
     }
 }
